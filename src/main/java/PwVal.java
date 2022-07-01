a56c361807572ded417c6973ec5de4934f20379a
yacinalkaissouni <yacin.alkaissouni@gmx.de>
2022-07-01 14:54:32 +0200

    PasswordNew

--- src/main/java/PwVal.java
+++ src/main/java/PwVal.java
@@ -2,30 +2,64 @@ public class PwVal {
 
     public static void main(String[] args) {
 
-        String pw = "Beispiel";
+        String pw = "Beispiel1";
 
-        boolean x = passwordVal(pw);
+        boolean correctPw = passwordVal(pw);
+        System.out.println("Das Password ist : " + correctPw);
 
-        System.out.println("Das Password ist : " + x);
+        boolean correctLength = checkLength(pw);
+        System.out.println("Die Länge des PW ist :" + correctLength);
+
+        boolean checkNumber = checkDigits(pw);
+        System.out.println("Das Passwort enthält Nummern ist : " +checkNumber);
 
     }
 
-    public static boolean passwordVal(String pw){
+    public static boolean passwordVal(String pw){ // Check richtiges Password
 
         boolean pwCorrect;
 
         if(pw == "Beispiel"){
-
             pwCorrect = true;
         }
         else{
-
             pwCorrect = false;
-
         }
-
         return pwCorrect;
 
     }
 
-}
+    //Check PW länge
+
+    public static boolean checkLength(String pw){
+
+        boolean lengthCorrect;
+
+        if(pw.length()>= 5){
+            lengthCorrect = true;
+        }
+        else{
+            lengthCorrect = false;
+        }
+
+        return lengthCorrect;
+    }
+
+
+
+    //Check zahlen
+    public static boolean checkDigits(String pass) {
+        char ch;
+        boolean ok = false;
+        for (int i = 0; i < pass.length() && !ok; i++) {
+            ch = pass.charAt(i);
+            if (Character.isDigit(ch)) {
+                ok = true;
+            }
+        }
+        return ok;
+    }
+
+    }
+
+
--- src/test/java/PwTest.java
+++ src/test/java/PwTest.java
@@ -1,18 +1,30 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Assumptions;
 import org.junit.jupiter.api.Test;
 
 public class PwTest {
 
-
     @Test
-
-    void pwTest() {
+    void pwLengthTest() {
 
         //given
-
+        String pw = "12345";
         //when
-
+        boolean actual = PwVal.checkLength(pw);
+        boolean expected = true;
         //then
+        Assertions.assertEquals(expected,actual);
+    }
 
+    @Test
+    void pwTest(){
 
+        //given
+        String pw ="Beispiel";
+        //when
+        boolean actual = PwVal.passwordVal(pw);
+        boolean expected = true;
+        //then
+        Assertions.assertEquals(expected,actual);
     }
 }
